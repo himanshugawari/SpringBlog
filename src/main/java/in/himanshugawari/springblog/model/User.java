58597be281770260d8ad63d22670d2141c52171a
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-09 21:51:52 +0530

    added model user using lombok for model with builder pattern

---
+++ src/main/java/in/himanshugawari/springblog/model/User.java
@@ -0,0 +1,43 @@
+package in.himanshugawari.springblog.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Username is required")
+	private String userName;
+
+	@NotBlank(message = "Password is required")
+	private String password;
+
+	@Email
+	@NotEmpty(message = "Email is required")
+	private String email;
+
+	private Instant created;
+
+	private boolean enabled;
+
+}
