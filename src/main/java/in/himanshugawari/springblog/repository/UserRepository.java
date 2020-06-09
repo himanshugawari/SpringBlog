176e9adf4a7d589623973782f15df028021500a4
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-09 22:44:23 +0530

    added userRepository

---
+++ src/main/java/in/himanshugawari/springblog/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package in.himanshugawari.springblog.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import in.himanshugawari.springblog.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUserName(String userName);
+
+}
