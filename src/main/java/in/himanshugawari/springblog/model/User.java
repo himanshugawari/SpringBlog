c9ab964b48e9f235fd3a8b9595df60d5f99ebe4d
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-09 22:44:04 +0530

    changed id touserId

--- src/main/java/in/himanshugawari/springblog/model/User.java
+++ src/main/java/in/himanshugawari/springblog/model/User.java
@@ -24,7 +24,7 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long userId;
 
 	@NotBlank(message = "Username is required")
 	private String userName;
