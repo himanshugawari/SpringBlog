ece4e268d56754d7dd5ced5db7b0212c36835293
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-11 22:07:53 +0530

    disabled csrf protection

--- src/main/java/in/himanshugawari/springblog/configuration/SecurityConfiguration.java
+++ src/main/java/in/himanshugawari/springblog/configuration/SecurityConfiguration.java
@@ -11,8 +11,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*http.csrf().disable();
-		http.headers().frameOptions().disable();
+		http.csrf().disable();
+		/*http.headers().frameOptions().disable();
 		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/h2-console/**")
 				.permitAll();
 		*/
